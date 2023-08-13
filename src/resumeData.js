6eb92b64be2226e4bd94e23d8daab54a3d62f77d
firaol tesfaye <firaoltesfaye81@gmail.com>
2023-08-13 21:12:43 +0300

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,99 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Firaol Tesfaye",
+    "role": "Frontend Developer and Data Scientist",
+    "linkedinId":"Firuol",
+    "skypeid": "",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about how to develop AI and machine learning technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/firaol-tesfaye-421436288",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/Firuol",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"instagram",
+          "url":"http://instagram.com/FIRAOL0112",
+          "className":"fa fa-instagram"
+        }
+      ],
+    "aboutme":"I am currently a 3rd  year student at Jimma University Jimma Institute of Technology. I am a self taught front-end Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Ethiopia, oromia, west shewa Ambo",
+    "website":"https://firuol.github.io",
+    "education":[
+      {
+        "UniversityName":"Jimma Institute Of Technology",
+        "specialization":"Software engineering",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2021",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"Jimma University",
+        "specialization":"Management",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2022",
+        "Achievements":""
+      }
+    ],
+   
+    "skillsDescription":"I have gained some skills in database management, networking and web development.",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+       {
+        "skillname":"mysql"
+      },
+      {
+        "skillname":"python"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Shop Locator & Price Tracer",
+        "description":"database project with front-end",
+        "imgurl":"images/shoplocator.png"
+      },
+      {
+        "name":"Edir Management system ",
+        "description":"Website",
+        "imgurl":"images/portfolio/project1.png"
+      },
+      {
+        "name":"smart car parking system",
+        "description":"website",  
+        "imgurl":"images/carparking.png"
+      },
+    ],
+    "testimonials":[
+      {
+        "description":"keep diging, until u get what u looking for not until u are tired!!!",
+        "name":"Tokkuma Abdisa"
+      },
+      {
+        "description":"you are one of the intelligent person i have seen in my life.",
+        "name":"Jibril Arbicho"
+      },
+      {
+        "description":"try try u can fly on the sky bro.",
+        "name":"Lalisa Bula"
+      }
+    ]
+  }
+  
+  export default resumeData
